193a212741429aa7ee5c0ffad7dd3e33c4962adc
bingchiao <37581982+bingchiao@users.noreply.github.com>
2020-06-01 13:30:29 +0800

    compile_mpi.sh
    the bash script for compiling MPI code

---
+++ complie_mpi.sh
@@ -0,0 +1,18 @@
+#usage: sh compile.sh filename.cpp
+
+# filename
+IN=${1}
+
+# your compiler
+CC=mpic++
+
+# openmp flag of your compiler
+OPENFLAG=-fopenmp
+#OPENFLAG=-openmp
+#OPENFLAG=-qopenmp
+
+# set the output name
+OUT=$(basename ${IN} .cpp).out
+
+${CC} ${OPENFLAG} ${IN} -o ${OUT}
+
