4439206e41049e319080c64c93d7c96a8938e6a5
WangsusPaulWu <fishbone99999999@gmail.com>
2020-06-10 18:59:21 +0800

    dao

--- Poisson.cpp
+++ Poisson.cpp
@@ -13,16 +13,17 @@ using namespace Array;
 using namespace fftwpp;
 
 double f(double x, double y){
-	return 1./sqrt((x+1)*(x+1)+(y+1)*(y+1));
+	return 1/(1-x+y*y);
 }
 double rho(double x, double y){
-	return 1/(1+x);
+	return x+y;
 }
 
 int main(){
 	fftw::maxthreads = 1;
     double Lx = 1.0, Ly = 1.0;
     double dx = Lx/(N), dy = Ly/(N);
+	double M = 0;
 
 	array2<Complex> rho_x(N,N,sizeof(Complex));
 	array2<Complex> phi_x(N,N,sizeof(Complex));
@@ -38,25 +39,26 @@ int main(){
 	}
 	for(int i = 0 ; i<N ; i++){
 		for(int j = 0 ; j<N ; j++){
-			rho_x(i,j) = (phi_x((i+1)%N,j)+phi_x((N+i-1)%N,j)+phi_x(i,(N+j-1)%N)+phi_x(i,(j+1)%N)-4.0*phi_x(i,j))/(dx*dx);
-			// rho_x(i,j) = rho(dx*i, dy*j);
+			// rho_x(i,j) = (phi_x((i+1)%N,j)+phi_x((N+i-1)%N,j)+phi_x(i,(N+j-1)%N)+phi_x(i,(j+1)%N)-4.0*phi_x(i,j))/(dx*dx);
+			rho_x(i,j) = rho(dx*i, dy*j);
+			// if(i==0||i==N-1||j==0||j==N-1) rho_x(i,j) = 0;
+			M += real(rho_x(i,j))*dx*dy;
 		}
 	}
 
-	cout << "f = \n";
-	cout << phi_x << endl;
-	cout << "rho_x = Lap f = \n";
+	// cout << "f = \n";
+	// cout << phi_x << endl;
+	cout << "rho_x = \n";
 	cout << rho_x << endl;	
 	
 	Forward.fft0(rho_x, rho_k);
 	cout << "rho_k = FFT(rho_x) = \n";
 	cout << rho_k << endl;
    	
-	cout << "Calculating phi_k...\n\n";
     for(int i = 0 ; i<N ; i++){
 		for(int j = 0 ; j<N ; j++){
-            phi_k(i,j) = -dx*dx*rho_k(i,j) / ( 4.0 * ( pow(sin(PI*min(i,N-i)/N),2.0) + pow(sin(PI*min(j,N-j)/N),2.0) ) );
-            if(i==0 && j==0) phi_k(i,j) = 0;
+            phi_k(i,j) = -dx*dx*rho_k(i,j) / ( 4.0 * ( pow(sin(PI*min(i,N-i)/N),2.0) + pow(sin(PI*min(j,N-j)/N),2.0) ) ) ;
+			if(i==0 && j==0) phi_k(i,j) = 0;
 		}
 	}
 	cout << "phi_k = \n";
@@ -66,17 +68,29 @@ int main(){
 	
     cout << "phi_x = IFFT(phi_k) = \n";
     cout << phi_x << endl;
-    
+
 	/* Output results */
     array2<Complex> Lap(N,N,sizeof(Complex));
     Lap *= 0;
 	for(int i = 0 ; i<N ; i++){
         for(int j = 0 ; j<N ; j++){
-            Lap(i,j) = (phi_x((i+1)%N,j)+phi_x((N+i-1)%N,j)+phi_x(i,(j+1)%N)+phi_x(i,(N+j-1)%N)-4.0*phi_x(i,j))/(dx*dx);
+            Lap(i,j) = (phi_x((i+1)%N,j)+phi_x((N+i-1)%N,j)+phi_x(i,(j+1)%N)+phi_x(i,(N+j-1)%N)-4.0*phi_x(i,j))/(dx*dx) - rho_x(i,j);
         }
     }
-    cout << "Lap phi_x = \n";
+    cout << "Lap phi_x - rho_x= \n";
 	cout << Lap << endl;
-    
+
+    array2<Complex> gx(N,N,sizeof(Complex));
+    gx *= 0;
+	for(int i = 0 ; i<N ; i++){
+        for(int j = 0 ; j<N ; j++){
+            gx(i,j) = (phi_x((i+1)%N,j)-phi_x((N+i-1)%N,j))/(2.*dx) - (f(dx*((i+1)%N),dy*j)-f(dx*((N+i-1)%N),dy*j))/(2.*dx);
+        }
+    }
+    cout << "gx - partial_x f = \n";
+	cout << gx << endl;
+	
+	cout << "M = " << M << endl;
+	
     return 0;
 }
